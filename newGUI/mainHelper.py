d67174d65aa695c24147a7fc9d077044d9466673
Mille Berg <bergm1@findlay.edu>
2025-03-23 19:39:24 -0400

    Dropdown menu for displaying answers

--- newGUI/mainHelper.py
+++ newGUI/mainHelper.py
@@ -27,20 +27,4 @@ def create_dropdown(parent, options, var, row, col, text = "Select an option:"):
     dropdown.grid(row=row, column=col + 1, padx=5, pady=5)
     return dropdown
 
-# Get answers based on selected question ID
-def get_answers(cnx, question_id):
-    c = cnx.cursor()
 
-    answer_ids = ['a', 'b', 'c', 'd', 'e']
-
-    answers = []
-
-    for answer_id in answer_ids:
-        concatenated_id = f"{question_id}{answer_id}" #Concatenate question ID
-        c.execute("SELECT choice_text FROM Question_Choices WHERE choice_id = %s", (concatenated_id,))
-        answer = c.fetchone()
-
-        if answer:
-            answers.append(answer[0]) # Add answer to the list
-
-    return answers
