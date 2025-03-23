8efc1d7a0bd89f268c18613512a870ce93315802
Bruce Keener <keenerb@findlay.edu>
2025-03-23 15:04:23 -0400

    Added a helper function python file and the icon imager for the top left corner. Note this should be on the c:/ directly or you change the path for every update.

---
+++ newGUI/mainHelper.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from tkinter import *
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        user='sql5764680', password='yK8gNIyhZm',
+        host='sql5.freesqldatabase.com', database='sql5764680'
+    )
+
+
+# Create a Function to Make the Back Button
+def create_back_button(parent, command):
+    btn = Button(parent, text="Back", command=command)
+    btn.grid(row=1, column=0, columnspan=1, pady=10, padx=10, ipadx=50)
+    return btn
